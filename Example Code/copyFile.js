dce90502726cee99b9008c86852e906d4ef0ec88
TAEYONG KIM <amico741@naver.com>
2021-08-11 17:58:47 +0900

    노드3장 정리한 것들 추가

--- Example Code/copyFile.js
+++ Example Code/copyFile.js
@@ -1,6 +1,6 @@
 const fs = require('fs').promises;
 
-fs.copyFile('readme4.txt', 'writeme4.txt')
+fs.copyFile('readme4.txt', 'writeme4.txt') // 노드 8.5 버전 이후에는 createReadStream과 createWriteStream을 pipe하지 않아도 파일 복사 가능.
     .then(() => {
         console.log('복사 완료');
     })
