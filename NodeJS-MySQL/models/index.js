56e8ccf81f652fbeaa2c2f18d4018cb49858d3b3
TAEYONG KIM <amico741@naver.com>
2022-04-17 03:17:03 +0900

    sequelize 구성 연결

--- NodeJS-MySQL/models/index.js
+++ NodeJS-MySQL/models/index.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const User = require('./user');
+const Comment = require('./comments');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env]; // config/config.json에서 데이터베이스 설정을 불러온 후에 new Sequelize를 통해 MySQL 연결 객체를 생성한다.
@@ -7,5 +9,14 @@ const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
+
+db.User = User;
+db.Comment = Comment;
+
+User.init(sequelize); // static 메서드 호출
+Comment.init(sequelize); // static 메서드 호출
+
+User.associate(db);
+Comment.associate(db);
 
 module.exports = db;
